b49cfd7d339871d0e9eaaee5b32eedd32cd4f86f
Myles Janik <mylesjanik@gmail.com>
2020-08-31 06:36:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  new_array = []]
+  new_array = []
   
   holiday_hash.each do |season, data|
     data.each do |holiday, supplies|
