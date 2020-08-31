954efeabd4c2230bbd2aaa5a1376c41122dd7b4f
Myles Janik <mylesjanik@gmail.com>
2020-08-31 05:48:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,6 +53,13 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  
+  holiday_hash.each do |season, data|
+    if season == ":winter"
+      return season.values
+    end
+  end
+    
 
 end
 
